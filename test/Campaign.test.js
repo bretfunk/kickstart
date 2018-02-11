eb7f2469362a2d64e819acdaeecea3504c2ef57a
BFunk <bret.e.funk@gmail.com>
2018-02-11 14:38:40 -0500

    few more tests, still bug with sending gas to manager request

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -43,11 +43,71 @@ describe('Campaign', async () => {
     assert.ok(await factory.options.address);
     assert.ok(await campaign.options.address);
   })
-});
-describe('basic functionality', async () => {
+
   it('has all accounts', async () => {
     assert.equal(accounts.length, 10);
   })
-})
+
+  it('new contract owner is the manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(manager, accounts[0]);
+  })
+
+  it('allows accounts to donate to campaign', async () => {
+    const doner = accounts[1];
+    await campaign.methods.contribute().send({
+      from: doner,
+      value: '1000000'
+    })
+    const total = await campaign.methods.approversCount().call()
+    assert.equal(total, 1);
+    const approver = await campaign.methods.approvers(doner).call()
+    assert.ok(approver)
+  })
+
+  it('campaign has minimium contribution', async () => {
+    const doner = accounts[1];
+    await campaign.methods.contribute().send({
+      from: doner,
+      value: '1000000'
+    })
+    const total = await campaign.methods.approversCount().call()
+    assert.equal(total, 1);
+
+    const lowDoner = accounts[2];
+
+    try {
+    await campaign.methods.contribute().send({
+      from: lowDoner,
+      value: '1'
+    })
+      assert(false)
+    } catch(error) {
+      assert(error);
+    }
+
+    //const newTotal = await campaign.methods.approversCount().call()
+    assert.equal(total, 1);
+
+  })
+
+  it('manager can make request', async () => {
+    await campaign.methods.createRequest(
+      "buy batteries",
+      "100",
+      accounts[9]
+    ).send({
+      from: accounts[0],
+      //gas: '0'
+    })
+
+    const request = await campaign.methods.requests(0).call();
+    assert.ok(request);
+    console.log(request);
+    //assert.equal(request.description, "buy batteries");
+    //assert.equal(request.value, 1);
+  })
+
+});
 
 
